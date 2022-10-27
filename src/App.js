a97799aa72c77275e3556de4c0e4b488efb6cb3e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-27 16:31:11 -0400

    playing with e2e

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css'
 
 function App() {
   const env = window.process?.env || process.env
-  console.log(env)
   return (
     <div className="App">
       <h1>{env.REACT_APP_TITLE}</h1>
-      <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
+      <h3>{env.REACT_APP_DESCRIPTION}</h3>
 
-      {process.env.NODE_ENV === 'development'
-        ? process.env.REACT_APP_DEV_MODE
-        : process.env.REACT_APP_PRO_MODE}
+      {env.NODE_ENV === 'development'
+        ? env.REACT_APP_DEV_MODE
+        : env.REACT_APP_PRO_MODE}
     </div>
   )
 }
